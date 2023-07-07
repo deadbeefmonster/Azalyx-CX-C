e7c95dc96188839538b10e6dfe422e9b97ec308d
Chris Humphries <chris@sogonsecurity.com>
2023-07-06 23:09:36 -0400

    Remove docker shell scripts and simply add the instructions to the README like a useful human would do

--- docker_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-docker build -t blitzkreig:ubuntu23.04 .
--- docker_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-mkdir -p linux_build
-docker run --rm -v "$(pwd):/blitzkrieg" --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -d --name blitzkrieg  -i blitzkreig:ubuntu23.04
