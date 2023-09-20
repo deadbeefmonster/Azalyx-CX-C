a41161fdd024c0bc99cc667599d08fef130b6b70
Christoff Humphries <christoff@deadbeef.monster>
2023-09-20 00:35:42 -0400

    Regenerate data structures for database usage based on updated SQL and perl script to generate them.

--- src/database_sqlite.h
+++ src/database_sqlite.h
@@ -14,96 +14,108 @@
 #include <glib/gi18n.h>
 #include <sqlite3.h>
 
+/* Table: envelope */
+struct database_data_envelope {
+    guint32 id;
+    GString *uuid;
+    GByteArray *data_bytes;
+    guint32 create_ts;
+};
+
+/* Table: envelope_body */
+struct database_data_envelope_body {
+    guint32 id;
+    GString *uuid;
+    GString *envelope_uuid; /* References database_envelope.uuid */
+    GByteArray *data_bytes;
+    guint32 create_ts;
+};
+
+/* Table: envelope_chunk */
+struct database_data_envelope_chunk {
+    guint32 id;
+    GString *envelope_uuid; /* References database_envelope.uuid */
+    guint32 start_byte_position;
+    guint32 chunk_size;
+    gboolean is_transferred;
+    gboolean is_acknowledged_required;
+    gboolean is_acknowledged;
+    guint32 create_ts;
+    guint32 transferred_ts;
+    guint32 acknowledged_ts;
+};
+
+/* Table: envelope_fault */
+struct database_data_envelope_fault {
+    guint32 id;
+    GString *uuid;
+    GString *envelope_uuid; /* References database_envelope.uuid */
+    GString *envelope_body_uuid;
+    GByteArray *data_bytes;
+    guint32 create_ts;
+};
+
+/* Table: envelope_headers */
+struct database_data_envelope_headers {
+    guint32 id;
+    GString *envelope_uuid; /* References database_envelope.uuid */
+    GString *header_key;
+    GString *header_value;
+    guint32 create_ts;
+};
+
+/* Table: log */
 struct database_data_log {
-    gint *id;
+    guint32 id;
     GString *message;
     GString *scope;
     GString *subject;
-    gint *create_ts;
+    guint32 create_ts;
 };
 
-struct database_data_session {
-    gint id;
-    GString *uuid;
-    GString *data;
-    gint create_ts;
-    gint update_ts;
-    gint expire_ts;
+/* Table: queue */
+struct database_data_queue {
+    guint32 id;
+    GString *session_uuid; /* References database_session.uuid */
+    GByteArray *data_bytes;
+    guint32 create_ts;
+    guint32 update_ts;
 };
 
-struct database_data_service {
-    gint id;
-    GString *name;
-    GString *type;
-    GString *proto;
-    gint port;
-    gboolean is_active;
-    gint create_ts;
-    gint update_ts;
-};
-
+/* Table: request_response */
 struct database_data_request_response {
-    GString *session_uuid;
-    GString *service_uuid;
+    guint32 id;
+    guint32 session_uuid; /* References database_session.uuid */
+    guint32 service_uuid; /* References database_service.uuid */
     GByteArray *request_bytes;
     GByteArray *response_bytes;
     GString *sender;
     GString *recipient;
-    gint create_ts;
-    gint update_ts;
+    guint32 create_ts;
+    guint32 update_ts;
 };
 
-struct database_data_envelope {
-    gint id;
+/* Table: service */
+struct database_data_service {
+    guint32 id;
     GString *uuid;
-    GByteArray *data_bytes;
-    gint create_ts;
+    GString *name;
+    GString *type;
+    GString *proto;
+    guint32 port;
+    gboolean is_active;
+    guint32 create_ts;
+    guint32 update_ts;
 };
 
-struct database_data_envelope_headers {
-    gint id;
-    GString *envelope_uuid; // database_data_envelope->uuid (one to many)
-    GString *header_key;
-    GString *header_value;
-    gint create_ts;
-};
-
-struct database_data_envelope_body {
-    gint id;
+/* Table: session */
+struct database_data_session {
+    guint32 id;
     GString *uuid;
-    GString *envelope_uuid; // database_data_envelope->uuid (one to many)
-    GByteArray *data_bytes;
-    gint create_ts;
-};
-
-struct database_data_envelope_fault {
-    gint id;
-    GString uuid;
-    GString envelope_uuid; // database_data_envelope->uuid (one to many)
-    GString envelope_body_uuid; // database_data_envelope_body->uuid
-    GByteArray data_bytes;
-    gint create_ts;
-};
-
-struct database_data_envelope_chunk {
-    gint64 id;
-    GString envelope_uuid; // database_data_envelope->uuid (one to many)
-    gint64 start_byte_position;
-    gint64 chunk_size;
-    gboolean is_transferred;
-    gboolean is_acknowledged_required;
-    gboolean is_acknowledged;
-    gint create_ts;
-    gint transferred_ts;
-    gint acknowledged_ts;
-};
-
-struct database_data_queue {
-    gint id;
-    GString *session_uuid; // database_data_session->uuid
-    GByteArray *data_bytes;
-    gint create_ts;
-    gint update_ts;
+    GString *data;
+    guint32 create_ts;
+    guint32 update_ts;
+    guint32 expire_ts;
 };
 
 
